5ca77aa13ed8712fdf408abb5ef0779a5ebcdbd0
Pramay Wankhade <pramaywankhade7@gmail.com>
2026-02-02 12:28:28 +0530

    feat: Add Vercel Analytics for tracking page views and user interactions

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useEffect } from "react";
 import { toast } from "sonner";
+import { Analytics } from "@vercel/analytics/react";
 import Index from "./pages/Index";
 import Send from "./pages/Send";
 import Receive from "./pages/ReceiveNew";
@@ -61,6 +62,7 @@ const App = () => (
         <Toaster />
         <Sonner />
         <AppContent />
+        <Analytics />
       </TooltipProvider>
     </QueryClientProvider>
   </ErrorBoundary>
