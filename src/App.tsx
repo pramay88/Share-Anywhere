5c09022200c452c044ef768fd5e078cc23d4a28b
Pramay Wankhade <pramaywankhade7@gmail.com>
2026-04-06 17:33:47 +0530

    feat: Vercel Speed Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useEffect } from "react";
 import { toast } from "sonner";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import { AuthProvider } from "@/contexts/AuthContext";
 import Index from "./pages/Index";
 import Send from "./pages/Send";
@@ -69,6 +70,7 @@ const App = () => (
           <Sonner />
           <AppContent />
           <Analytics />
+          <SpeedInsights />
         </TooltipProvider>
       </AuthProvider>
     </QueryClientProvider>
